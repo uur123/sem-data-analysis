b538994c315f5126addd7b21ca004ae1859fe3ed
Ugur Kayran <ugurkayran@ugurs-imac.home>
2022-12-27 15:14:58 +0100

    changed background

--- st_aspex_pass.py
+++ st_aspex_pass.py
@@ -6,6 +6,10 @@ import altair as alt
 import pickle
 from pathlib import Path
 
+st.set_page_config(page_title= "Aspex Data",
+                    page_icon = ":bar_chart:",
+                    layout = "wide"
+)
 
 
 st.header(" Aspex data analysis web-app ")
