1ee91a360dc94147a42ce49ee500276faad36f29
uur123 <kayran.ugur@gmail.com>
2023-04-25 21:42:19 +0200

    download_button

--- st_aspex_pass.py
+++ st_aspex_pass.py
@@ -232,6 +232,18 @@ if authentication_status:
 
         st.caption('Comparison of the Grain size measurement distribution')
         st.altair_chart(chart_grains, use_container_width=True)
+    
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    
+    csv = convert_df(new_df)
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='data.csv',
+        mime='text/csv',
+        )
 
 
 footer="""<style>
